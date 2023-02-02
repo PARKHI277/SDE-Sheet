b805b3784d9cca674a22b146a62514e8edbe4d0a
Parkhi <parkhigarg277@gmail.com>
2023-02-02 11:55:40 +0530

    update

---
+++ Day1/sortColors.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0;
+        int mid = 0;
+        int high = n-1;
+        while(mid <= high)
+        {
+            switch(nums[mid])
+            {
+                case 0 :
+                 swap(nums[lo++],nums[mid++]);
+                 break;
+
+                case 1 : 
+                  mid++;
+                  break;
+
+                  case 2 :
+                   swap(nums[mid],nums[high--]);
+                   break;
+            }
+        }
+    }
+};
