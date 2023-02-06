7d1837d2d50ce235a520e6d4927a0161566dd6d3
Parkhi <parkhigarg277@gmail.com>
2023-02-07 00:50:56 +0530

    update

---
+++ Day3/majorityElement2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+vector < int > majorityElement(int arr[], int n) {
+  unordered_map < int, int > mp;
+  vector < int > ans;
+
+  for (int i = 0; i < n; i++) {
+    mp[arr[i]]++;
+  }
+
+  for (auto x: mp) {
+    if (x.second > (n / 3))
+      ans.push_back(x.first);
+  }
+
+  return ans;
+}
