df7b16695bbbddf5c97e6f15975cd8da830d6962
Parkhi <parkhigarg277@gmail.com>
2023-02-07 01:18:11 +0530

    uniquePath

---
+++ Day3/uniquePath.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+int countPath(int i ,int j , int m,int n,vector<vector<int>>&dp)
+{
+    if( i == (m-1) && j == (n-1))
+    {
+        return 1;
+    }
+    if( i>m || j > n)
+    {
+        return 0;
+    }
+    if(dp[i][j] != -1)
+    {
+        return dp[i][j];
+    }
+    else
+    {
+        return dp[i][j] = countPath(i+1,j,m,n,dp) + countPath(i,j+1,m,n,dp);
+    }
+
+}
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        int num = countPath(0,0,m,n,dp);
+       return num;
+    }
+};
