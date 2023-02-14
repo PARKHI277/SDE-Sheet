e98d589db1130c561fbaf701907801f749c1e663
Parkhi <parkhigarg277@gmail.com>
2023-02-14 23:40:20 +0530

    intersection

---
+++ Day6/intersection.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    int find(ListNode*head1 , ListNode*head2)
+    {
+        int l1 = 0;
+        int l2 = 0;
+        while(head1 != NULL || head2 != NULL)
+        {
+            if(head1 != NULL)
+            {
+                l1++;
+                head1 = head1->next;
+            }
+            if(head2 != NULL)
+            {
+                l2++;
+                head2 = head2->next;
+            }
+            
+           
+        }
+        return l1-l2;
+    }
+    
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+        int diff = find(head1,head2);
+        if(diff < 0)
+        {
+            while(diff++ != 0)
+            {
+                head2 = head2->next;
+            }
+        }
+        else
+        {
+            while(diff-- !=0)
+            {
+                head1 =  head1->next;
+            }
+        }
+        
+        while(head1 != NULL)
+        {
+            if(head1 == head2)
+            {
+                return head1;
+            }
+            head2 = head2->next;
+            head1 = head1->next;
+        }
+        return head1;
+    }
+};
+
