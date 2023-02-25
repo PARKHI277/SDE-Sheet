3bd4c66ddc4500e924241948fb11714b7d87f9cf
Parkhi <parkhigarg277@gmail.com>
2023-02-25 23:27:08 +0530

    removeduplicates

---
+++ Day7/removeDuplicates.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+         int j = 0;
+        int count  =  1;
+        for(int i = 1;i<nums.size();i++)
+        {
+            if(nums[i] != nums[i-1])
+            {
+                count++;
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return count;
+    }
+};
