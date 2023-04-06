56a1dae2203bcbb124f0b58312fde3ced4784b0f
Parkhi <parkhigarg277@gmail.com>
2023-04-06 12:36:04 +0530

    update

---
+++ Day15/reverse.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+     string tmp="", ans="";
+    
+        for(int i=0;i<s.size();i++)
+        {
+            char ch = s[i];
+            if(ch==' ')
+            {
+                if(tmp!="")
+                    ans = tmp + " " + ans;
+                tmp = "";        
+            }
+            else 
+                tmp += ch;
+        }
+        
+        if(tmp!="")
+            ans = tmp + " " + ans;
+        ans.pop_back();
+        
+        return ans;      
+    }
+        
+    
+};
